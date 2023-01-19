4d66579034d37bbdc219e5a34650593d971bea46
Kevin Kraiss <kevinkraiss@gmail.com>
2023-01-18 20:58:01 -0600

    initial commit

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,25 @@
+// TODO: Install the following package:
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+// TODO: Complete the initDb() function below:
+const initdb = async () => {
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+// TODO: Complete the postDb() function below:
+export const postDb = async (name, home, cell, email)  => {
+ 
+};
+
+// TODO: Complete the getDb() function below:
+export const getDb = async () => {
+  
+};
+
+// TODO: Complete the deleteDb() function below:
+export const deleteDb = async (id) => {
+  
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      cards: './src/js/cards.js'
     },
+
+    // TODO: Add the correct output
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
       
+    },
+
+    // TODO: Add the correct plugins
+    plugins: [
+     
     ],
 
+    // TODO: Add the correct modules
     module: {
-      rules: [
-        
-      ],
-    },
+
+    }
   };
 };
