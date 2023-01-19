cbe05f98eb4db913505c9a6c61829f048f5f9e9f
Kevin Kraiss <kevinkraiss@gmail.com>
2023-01-18 21:42:41 -0600

    set up npm scripts and configured some webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,62 @@
 // TODO: Install the following package:
 import { openDB } from 'idb';
 
+const dbName = 'contacts-db'
+const storeName = 'contacts'
+
+
 // TODO: Complete the initDb() function below:
 const initdb = async () => {
-
+    await openDB(dbName, 1, {
+        upgrade(db) {
+            db.createObjectStore(storeName, { 
+                keyPath: 'id', 
+                autoIncrement: true 
+            })
+        }
+    })
 };
 
 
 // TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+    // open db
+    const db = await openDB(dbName, 1)
+    // create tx
+    const tx = db.transaction(storeName, 'readwrite')
+    // access store from tx
+    const store = tx.objectStore(storeName)
+    // mutate store
+    await store.add({
+        name,
+        home_phone: home,
+        cell_phone: cell,
+        email,
+    })
 };
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+    // open db
+    const db = await openDB(dbName, 1)
+    // create tx
+    const tx = db.transaction(storeName, 'readwrite')
+    // access store from tx
+    const store = tx.objectStore(storeName)
+    // mutate store
+    return await store.getAll()
 };
 
 // TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+    // open db
+    const db = await openDB(dbName, 1)
+    // create tx
+    const tx = db.transaction(storeName, 'readwrite')
+    // access store from tx
+    const store = tx.objectStore(storeName)
+    // mutate store
+    await store.delete(id)
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const  WebpackPwaManifest = require('webpack-pwa-manifest')
+const {GenerateSW} = require('workbox-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = () => {
   return {
@@ -11,17 +14,25 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js'
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HTMLWebpackPlugin({
+        template: './index.html'
+      })
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
     }
   };
 };
